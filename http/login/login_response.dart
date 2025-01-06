d17c35ba9ae018b86931f169eee939fdfc6a31ba
gmacgre <gmacgre@byu.edu>
2025-01-05 21:19:50 -0600

    Improve LoginResponse

--- http/login/login_response.dart
+++ http/login/login_response.dart
@@ -2,4 +2,10 @@ import '../response.dart';
 
 class LoginResponse extends TIResponse {
   LoginResponse(): super('Login');
+  Map<String, dynamic> toJson() => {
+    'type': type,
+  };
+  factory LoginResponse.fromJson(Map<String, dynamic> json) {
+    return LoginResponse();
+  }
 }
