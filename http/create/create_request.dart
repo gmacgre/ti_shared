8d43117d13ac593c87c4200bf78cd00844beb188
gmacgre <gmacgre@byu.edu>
2025-01-11 11:10:12 -0600

    Generalize TIResponse Add CreateRequest

---
+++ http/create/create_request.dart
@@ -0,0 +1,12 @@
+import '../request.dart';
+
+class CreateRequest extends TIRequest {
+  String userId;
+  String userPassword;
+  String gameId;
+  String gamePassword;
+  CreateRequest(String uuid, this.userId, this.userPassword, this.gameId, this.gamePassword) : super('Create', uuid);
+  factory CreateRequest.fromJson(Map<String, dynamic> json) {
+    return CreateRequest(json['uuid'], json['userId'], json['userPassword'], json['gameId'], json['gamePassword']);
+  }
+}
--- http/response.dart
+++ http/response.dart
@@ -1,6 +1,20 @@
-abstract class TIResponse {
+class TIResponse {
   String type;
   String uuid;
   bool isSuccess;
+  String? message;
   TIResponse(this.type, this.uuid, this.isSuccess);
+  TIResponse.withMessage(this.type, this.uuid, this.isSuccess, this.message);
+  Map<String, dynamic> toJson() => {
+    'type': type,
+    'uuid': uuid,
+    'isSuccess': isSuccess,
+    'message': message
+  };
+  factory TIResponse.fromJson(Map<String, dynamic> json) {
+    if(json['message'] != null) {
+      return TIResponse.withMessage(json['type'], json['uuid'], json['isSuccess'], json['message']);
+    }
+    return TIResponse(json['type'], json['uuid'], json['isSuccess']);
+  }
 }
