0d81705338190e69f4913d82b3ac87dc65f7f620
gmacgre <gmacgre@byu.edu>
2025-01-11 10:12:37 -0600

    Add Parameters to LoginRequest

--- http/login/login_request.dart
+++ http/login/login_request.dart
@@ -1,12 +1,21 @@
 import '../request.dart';
 
 class LoginRequest extends TIRequest {
-  LoginRequest(String uuid) : super('Login', uuid);
+  String userId;
+  String userPassword;
+  String gameId;
+  String gamePassword;
+
+  LoginRequest(String uuid, this.userId, this.userPassword, this.gameId, this.gamePassword) : super('Login', uuid);
   Map<String, dynamic> toJson() => {
     'type': type,
-    'uuid': uuid
+    'uuid': uuid,
+    'userId': userId,
+    'userPassword': userPassword,
+    'gameId': gameId,
+    'gamePassword': gamePassword
   };
   factory LoginRequest.fromJson(Map<String, dynamic> json) {
-    return LoginRequest(json['uuid']);
+    return LoginRequest(json['uuid'], json['userId'], json['userPassword'], json['gameId'], json['gamePassword']);
   }
 }
