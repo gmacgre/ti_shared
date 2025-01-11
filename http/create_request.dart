65c9eab489f93fddd6264f597d4602e52d0cd0e7
gmacgre <gmacgre@byu.edu>
2025-01-11 13:05:55 -0600

    Finalize Create and Login Requests

--- http/create_request.dart
+++ http/create_request.dart
@@ -1,4 +1,4 @@
-import '../request.dart';
+import 'request.dart';
 
 class CreateRequest extends TIRequest {
   String userId;
--- http/login_request.dart
+++ http/login_request.dart
@@ -1,20 +1,11 @@
-import '../request.dart';
+import 'request.dart';
 
 class LoginRequest extends TIRequest {
   String userId;
   String userPassword;
   String gameId;
   String gamePassword;
-
   LoginRequest(String uuid, this.userId, this.userPassword, this.gameId, this.gamePassword) : super('Login', uuid);
-  Map<String, dynamic> toJson() => {
-    'type': type,
-    'uuid': uuid,
-    'userId': userId,
-    'userPassword': userPassword,
-    'gameId': gameId,
-    'gamePassword': gamePassword
-  };
   factory LoginRequest.fromJson(Map<String, dynamic> json) {
     return LoginRequest(json['uuid'], json['userId'], json['userPassword'], json['gameId'], json['gamePassword']);
   }
